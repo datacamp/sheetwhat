61b13bef060c93cd5c8f9046e164730752ebdba9
vincentvankrunkelsven <vincent@datacamp.com>
2018-09-10 10:27:48 +0200

    feat(charts): add basic functionality and fix test

--- sheetwhat/checks/check_pivot.py
+++ sheetwhat/checks/check_pivot.py
@@ -224,3 +224,79 @@ def has_equal_pivot(state, extra_msg=None):
                     f"at `{child.sct_range}`:\n\n{_issues_msg}\n"
                 )
                 child.do_test(_msg)
+
+
+def infer_chart_type(chart):
+    chart_types = [
+        "basicChart",
+        "pieChart",
+        "bubbleChart",
+        "candlestickChart",
+        "orgChart",
+        "histogramChart",
+        "waterfallChart",
+        "treemapChart",
+    ]
+    for chart_type in chart_types:
+        if safe_glom(chart, f"spec.{chart_type}") is not None:
+            return chart_type
+
+
+def has_equal_chart(state, extra_msg=None):
+    solution_chart = state.solution_data["charts"][0]
+    if len(state.student_data["charts"]) == 0:
+        state.do_test("Please create a chart.")
+    student_chart = state.student_data["charts"][0]
+    issues = []
+    bound_rules = {
+        key: RuleClass(student_chart, solution_chart, issues)
+        for key, RuleClass in rule_types.items()
+    }
+    bound_rules["existence"]("spec.title", "There is no title."),
+    bound_rules["equality"]("spec.title", "The title is not correct."),
+    bound_rules["existence"]("spec.subTitle", "There is no subtitle.")
+    bound_rules["equality"]("spec.subTitle", "The subtitle is not correct.")
+
+    # Figure out chart type
+    solution_chart_type = infer_chart_type(solution_chart)
+
+    bound_rules["existence"](
+        f"spec.{solution_chart_type}", "The chart type is not correct."
+    )
+
+    if solution_chart_type == "basicChart":
+        bound_rules["equality"](
+            "spec.basicChart.chartType", "The chart type is not correct."
+        )
+
+    if len(issues) == 0:
+        #         import pdb; pdb.set_trace()
+        bound_rules["existence"](
+            f"spec.{solution_chart_type}.domains", "There is no XYZ specified."
+        )
+        bound_rules["array_equality"](
+            f"spec.{solution_chart_type}.domains", "The {ordinal} XYZ is not correct."
+        )
+        bound_rules["existence"](
+            f"spec.{solution_chart_type}.series", "There are no series specified."
+        )
+        bound_rules["array_equal_length"](
+            f"spec.{solution_chart_type}.series",
+            (
+                "The number of series is incorrect. "
+                "Expected {expected}, but got {actual}."
+            ),
+        )
+        bound_rules["array_equality"](
+            f"spec.{solution_chart_type}.series", ("The {ordinal} series is incorrect.")
+        )
+
+    nb_issues = len(issues)
+    if nb_issues > 0:
+        _issues_msg = "\n".join([f"- {issue}" for issue in issues])
+        _msg = (
+            f"There {'are' if nb_issues > 1 else 'is'} {nb_issues} "
+            f"issue{'s' if nb_issues > 1 else ''} with the chart:"
+            f"\n\n{_issues_msg}\n"
+        )
+        state.do_test(_msg)
---
+++ tests/test_has_equal_chart.py
@@ -0,0 +1,361 @@
+import pytest
+from copy import deepcopy
+from helper import (
+    Identity,
+    Mutation,
+    setup_state,
+    verify_success,
+    compose,
+    Deletion,
+    Addition,
+)
+
+from sheetwhat.checks import has_equal_chart
+
+# Fixtures
+@pytest.fixture()
+def charts():
+    return [
+        {
+            "chartId": 873985484,
+            "spec": {
+                "title": "Adams, Baker, Clark and Davis",
+                "basicChart": {
+                    "chartType": "AREA",
+                    "legendPosition": "RIGHT_LEGEND",
+                    "axis": [
+                        {
+                            "position": "BOTTOM_AXIS",
+                            "title": "Day",
+                            "format": {"fontFamily": "Roboto"},
+                        },
+                        {"position": "LEFT_AXIS"},
+                    ],
+                    "domains": [
+                        {
+                            "domain": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 0,
+                                            "endColumnIndex": 1,
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    "series": [
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 1,
+                                            "endColumnIndex": 2,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 2,
+                                            "endColumnIndex": 3,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 3,
+                                            "endColumnIndex": 4,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 4,
+                                            "endColumnIndex": 5,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                    ],
+                    "headerCount": 1,
+                    "stackedType": "STACKED",
+                },
+                "hiddenDimensionStrategy": "SKIP_HIDDEN_ROWS_AND_COLUMNS",
+                "titleTextFormat": {"fontFamily": "Roboto"},
+                "fontName": "Roboto",
+            },
+            "position": {
+                "overlayPosition": {
+                    "anchorCell": {"sheetId": "Sheet1", "columnIndex": 6},
+                    "offsetXPixels": 3,
+                    "widthPixels": 600,
+                    "heightPixels": 371,
+                }
+            },
+        },
+        {
+            "chartId": 1653050700,
+            "spec": {
+                "basicChart": {
+                    "chartType": "COLUMN",
+                    "legendPosition": "RIGHT_LEGEND",
+                    "axis": [{"position": "BOTTOM_AXIS"}, {"position": "LEFT_AXIS"}],
+                    "domains": [
+                        {
+                            "domain": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 0,
+                                            "endColumnIndex": 1,
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    "series": [
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 1,
+                                            "endColumnIndex": 2,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 2,
+                                            "endColumnIndex": 3,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 3,
+                                            "endColumnIndex": 4,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                        {
+                            "series": {
+                                "sourceRange": {
+                                    "sources": [
+                                        {
+                                            "startRowIndex": 0,
+                                            "endRowIndex": 8,
+                                            "startColumnIndex": 4,
+                                            "endColumnIndex": 5,
+                                        }
+                                    ]
+                                }
+                            },
+                            "targetAxis": "LEFT_AXIS",
+                        },
+                    ],
+                },
+                "hiddenDimensionStrategy": "SKIP_HIDDEN_ROWS_AND_COLUMNS",
+                "fontName": "Roboto",
+            },
+            "position": {
+                "overlayPosition": {
+                    "anchorCell": {"sheetId": "Sheet1"},
+                    "widthPixels": 600,
+                    "heightPixels": 371,
+                }
+            },
+        },
+    ]
+
+
+@pytest.fixture()
+def solution_data(charts):
+    return {
+        "values": [[1, 1, 1], [1, 52, 8]],
+        "formulas": [["=0+1", 1, 1], ["=1+0", "=52", 8]],
+        "charts": charts,
+    }
+
+# Tests
+@pytest.mark.parametrize(
+    "trans, correct",
+    [
+        (Identity(), True),
+        (Mutation(["charts"], []), False),
+        (Mutation(["charts", 0, "spec", "basicChart", "chartType"], "LINE"), False),
+        (Mutation(["charts", 0, "spec", "title"], "Other title"), False),
+        (Deletion(["charts", 0, "spec", "title"]), False),
+        (Mutation(["charts", 0, "spec", "subTitle"], "something"), False),
+        (Deletion(["charts", 0, "spec", "basicChart", "domains", 0]), False),
+        (
+            Mutation(
+                [
+                    "charts",
+                    0,
+                    "spec",
+                    "basicChart",
+                    "domains",
+                    0,
+                    "domain",
+                    "sourceRange",
+                    "sources",
+                    0,
+                ],
+                {
+                    "startRowIndex": 1,
+                    "endRowIndex": 8,
+                    "startColumnIndex": 1,
+                    "endColumnIndex": 2,
+                },
+            ),
+            False,
+        ),
+        (Deletion(["charts", 0, "spec", "basicChart", "series", 0]), False),
+        (
+            Addition(
+                [
+                    "charts",
+                    0,
+                    "spec",
+                    "basicChart",
+                    "series",
+                    0,
+                    "series",
+                    "sourceRange",
+                    "sources",
+                ],
+                {
+                    "startRowIndex": 1,
+                    "endRowIndex": 8,
+                    "startColumnIndex": 5,
+                    "endColumnIndex": 6,
+                },
+            ),
+            False,
+        ),
+        (
+            Mutation(
+                [
+                    "charts",
+                    0,
+                    "spec",
+                    "basicChart",
+                    "series",
+                    0,
+                    "series",
+                    "sourceRange",
+                    "sources",
+                    0,
+                ],
+                {
+                    "startRowIndex": 1,
+                    "endRowIndex": 8,
+                    "startColumnIndex": 5,
+                    "endColumnIndex": 6,
+                },
+            ),
+            False,
+        ),
+    ],
+)
+def test_check_charts(solution_data, trans, correct):
+    user_data = trans(deepcopy(solution_data))
+    # sct_range is irrelevant in charts
+    s = setup_state(user_data, solution_data, "A1")
+    with verify_success(correct):
+        has_equal_chart(s)
+
+
+@pytest.mark.parametrize(
+    "trans, correct",
+    [
+        (
+            compose(
+                Deletion(["charts", 0, "spec", "basicChart"]),
+                Mutation(
+                    ["charts", 0, "spec", "pieChart"],
+                    {
+                        "domain": {
+                            "sourceRange": {
+                                "sources": [
+                                    {
+                                        "sheetId": 1964080503,
+                                        "startRowIndex": 0,
+                                        "endRowIndex": 4,
+                                        "startColumnIndex": 0,
+                                        "endColumnIndex": 1,
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                ),
+            ),
+            False,
+        )
+    ],
+)
+def test_check_charts_trans_on_solution(solution_data, trans, correct):
+    user_data = deepcopy(solution_data)
+    solution_data = trans(deepcopy(solution_data))
+    # sct_range is irrelevant in charts
+    s = setup_state(user_data, solution_data, "A1")
+    with verify_success(correct):
+        has_equal_chart(s)
