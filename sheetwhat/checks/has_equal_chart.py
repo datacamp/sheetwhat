f5b8e5facd5722205d2edaa766dfe7f40511e51d
vincentvankrunkelsven <vincent@datacamp.com>
2018-11-29 23:53:46 +0100

    feat: add tests, minor style changes

--- sheetwhat/checks/has_equal_chart.py
+++ sheetwhat/checks/has_equal_chart.py
@@ -19,19 +19,19 @@ def infer_chart_type(chart):
             return chart_type
 
 
-def manhatten_distance_to_chart(chart, sct_range_str):
+def manhatten_distance_to_chart(chart, sct_range):
     anchor_cell = safe_glom(chart, "position.overlayPosition.anchorCell")
     x = safe_glom(anchor_cell, "columnIndex", 0)
     y = safe_glom(anchor_cell, "rowIndex", 0)
-    sct_range = range_to_row_columns(sct_range_str)
-    x_sct = safe_glom(sct_range, "start_column", 0)
-    y_sct = safe_glom(sct_range, "start_row", 0)
+    sct_range_as_row_columns = range_to_row_columns(sct_range)
+    x_sct = safe_glom(sct_range_as_row_columns, "start_column", 0)
+    y_sct = safe_glom(sct_range_as_row_columns, "start_row", 0)
     return abs(x - x_sct) + abs(y - y_sct)
 
 
-def find_chart(sct_range, charts):
+def find_chart(charts, sct_range):
     distance_to_sct = functools.partial(
-        manhatten_distance_to_chart, sct_range_str=sct_range
+        manhatten_distance_to_chart, sct_range=sct_range
     )
     distances = [distance_to_sct(chart) for chart in charts]
     min_distance_index = distances.index(min(distances))
@@ -39,18 +39,16 @@ def find_chart(sct_range, charts):
 
 
 def has_equal_chart(state, extra_msg=None):
-    solution_chart = find_chart(state.sct_range, state.solution_data["charts"])
+    solution_chart = find_chart(state.solution_data["charts"], state.sct_range)
     if len(state.student_data["charts"]) == 0:
-        state.do_test("Please create a chart.")
-    student_chart = find_chart(state.sct_range, state.student_data["charts"])
+        state.do_test(f"Please create a chart near `{state.sct_range}`.")
+    student_chart = find_chart(state.student_data["charts"], state.sct_range)
     issues = []
     bound_rules = {
         key: RuleClass(student_chart, solution_chart, issues)
         for key, RuleClass in rule_types.items()
     }
-    bound_rules["existence"]("spec.title", "There is no title."),
     bound_rules["equality"]("spec.title", "The title is not correct."),
-    bound_rules["existence"]("spec.subTitle", "There is no subtitle.")
     bound_rules["equality"]("spec.subTitle", "The subtitle is not correct.")
 
     # Figure out chart type
@@ -66,15 +64,10 @@ def has_equal_chart(state, extra_msg=None):
         )
 
     if len(issues) == 0:
-        bound_rules["existence"](
-            f"spec.{solution_chart_type}.domains", "There is no XYZ specified."
+        bound_rules["equality"](
+            f"spec.{solution_chart_type}.domains", "The X-axis is not correct."
         )
-        bound_rules["array_equality"](
-            f"spec.{solution_chart_type}.domains", "The {ordinal} XYZ is not correct."
-        )
-        bound_rules["existence"](
-            f"spec.{solution_chart_type}.series", "There are no series specified."
-        )
+
         bound_rules["array_equal_length"](
             f"spec.{solution_chart_type}.series",
             (
@@ -91,7 +84,7 @@ def has_equal_chart(state, extra_msg=None):
         _issues_msg = "\n".join([f"- {issue}" for issue in issues])
         _msg = (
             f"There {'are' if nb_issues > 1 else 'is'} {nb_issues} "
-            f"issue{'s' if nb_issues > 1 else ''} with the chart:"
-            f"\n\n{_issues_msg}\n"
+            f"issue{'s' if nb_issues > 1 else ''} with the chart "
+            f"close to `{state.sct_range}`:\n\n{_issues_msg}\n"
         )
         state.do_test(_msg)
