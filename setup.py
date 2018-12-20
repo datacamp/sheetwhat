b818850d1575035b05f4997c8e74dd5b60fefcd2
Jeroen Hermans <jeroen@datacamp.com>
2018-12-20 13:26:08 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -5,38 +5,43 @@ import ast
 from os import path
 from setuptools import setup
 
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
-
 PACKAGE_NAME = "sheetwhat"
 REQUIREMENT_NAMES = ["protowhat", "glom"]
 
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, "README.md"), encoding="utf-8") as fp:
-    README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), encoding="utf-8") as fp:
+VERSION_FILE = path.join(HERE, "src", PACKAGE_NAME, "__init__.py")
+REQUIREMENTS_FILE = path.join(HERE, "requirements.txt")
+README_FILE = path.join(HERE, "README.md")
+
+with open(VERSION_FILE, encoding="utf-8") as fp:
     _version_re = re.compile(r"__version__\s+=\s+(.*)")
-    VERSION = str(ast.literal_eval(_version_re.search(
-        fp.read()).group(1)))
-with open(path.join(HERE, "requirements.txt"), encoding="utf-8") as fp:
+    VERSION = str(ast.literal_eval(_version_re.search(fp.read()).group(1)))
+with open(REQUIREMENTS_FILE, encoding="utf-8") as fp:
     req_txt = fp.read()
-    _requirements_re_template = r"^({}[\s<>=]*.*)$"
+    _requirements_re_template = r"^({}(?:\s*[<>=]+\s*\S*)?)\s*(?:#.*)?$"
     REQUIREMENTS = [
         re.search(_requirements_re_template.format(requirement), req_txt, re.M).group(0)
         for requirement in REQUIREMENT_NAMES
     ]
+with open(README_FILE, encoding="utf-8") as fp:
+    README = fp.read()
 
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
-    packages=["sheetwhat", "sheetwhat.checks"],
+    packages=[PACKAGE_NAME, "sheetwhat.checks"],
     install_requires=REQUIREMENTS,
     description="Submission correctness tests for spreadsheets",
     long_description=README,
     long_description_content_type="text/markdown",
-    license="GNU version 3",
     author="Vincent Vankrunkelsven",
     author_email="vincent@datacamp.com",
     maintainer="Jeroen Hermans",
     maintainer_email="content-engineering@datacamp.com",
     url="https://github.com/datacamp/sheetwhat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
 )
